b7c723c3c38c2c2ca9eb7067e34fb526052cfd34
Beginning Android <udacityandroiddemo@gmail.com>
2016-02-20 21:49:11 -0800

    1.10 Use for loop to add TextViews to the screen for each word in the list

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -45,12 +45,11 @@ public class NumbersActivity extends AppCompatActivity {
         // Find the root view so we can add child views to it
         LinearLayout rootView = (LinearLayout) findViewById(R.id.rootView);
 
-        // Create a variable to keep track of the current index position
-        int index = 0;
-
         // Keep looping until we've reached the end of the list (which means keep looping
-        // as long as the current index position is less than the length of the list)
-        while (index < words.size()) {
+        // as long as the current index position is less than the length of the list).
+        // The index variable keeps track of our current position in the list and
+        // increments by 1 each time the code in the loop is executed.
+        for (int index = 0; index < words.size(); index++) {
             // Create a new TextView
             TextView wordView = new TextView(this);
 
@@ -59,9 +58,6 @@ public class NumbersActivity extends AppCompatActivity {
 
             // Add this TextView as another child to the root view of this layout
             rootView.addView(wordView);
-
-            // Increment the index variable by 1
-            index++;
         }
     }
 }
