b09aa1f9e991a4d2d236aa5728a3e7b5c4ae465d
Beginning Android <udacityandroiddemo@gmail.com>
2016-03-01 18:04:06 -0800

    1.7 Add log messages for the words array in NumbersActivity

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -15,8 +15,9 @@
  */
 package com.example.android.miwok;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -37,5 +38,17 @@ public class NumbersActivity extends AppCompatActivity {
         words[7] = "eight";
         words[8] = "nine";
         words[9] = "ten";
+
+        // Verify the contents of the array by printing out each array element to the logs
+        Log.v("NumbersActivity", "Word at index 0: " + words[0]);
+        Log.v("NumbersActivity", "Word at index 1: " + words[1]);
+        Log.v("NumbersActivity", "Word at index 2: " + words[2]);
+        Log.v("NumbersActivity", "Word at index 3: " + words[3]);
+        Log.v("NumbersActivity", "Word at index 4: " + words[4]);
+        Log.v("NumbersActivity", "Word at index 5: " + words[5]);
+        Log.v("NumbersActivity", "Word at index 6: " + words[6]);
+        Log.v("NumbersActivity", "Word at index 7: " + words[7]);
+        Log.v("NumbersActivity", "Word at index 8: " + words[8]);
+        Log.v("NumbersActivity", "Word at index 9: " + words[9]);
     }
 }
