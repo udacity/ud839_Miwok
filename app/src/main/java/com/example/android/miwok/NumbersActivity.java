5f9727c5abbb681f00036c1606cf56bd73c75dae
dcman <justin.johnson.90@my.csun.edu>
2016-09-02 22:14:53 -0700

    Custom Word Class Quiz

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -17,11 +17,8 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.ArrayAdapter;
-import android.widget.LinearLayout;
 import android.widget.ListView;
-import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -46,7 +43,7 @@ public class NumbersActivity extends AppCompatActivity {
         words.add("Ten");
 
         // Find the root view so we can add child views to it
-        ArrayAdapter<String> itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, words);
+        ArrayAdapter<String> itemsAdapter = new ArrayAdapter<>(this, R.layout.list_item, words);
 
         ListView listView = (ListView) findViewById(R.id.list);
 
