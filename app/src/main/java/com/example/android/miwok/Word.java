59c60fff78fece1337f5677ae6f7bc81be6c42ee
Guilherme Fainer Dalben <guilherme.fainer@gmail.com>
2017-08-17 19:45:07 -0300

    Add images for the words

--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -8,11 +8,21 @@ public class Word {
 
     private String mDefaultTranslation;
     private String mMiwokTranslation;
+    private int mImageId;
 
-    public Word(String defaultTranslation, String miwokTranslation)
-    {
-        mDefaultTranslation=defaultTranslation;
-        mMiwokTranslation=miwokTranslation;
+    public Word(String defaultTranslation, String miwokTranslation) {
+        mDefaultTranslation = defaultTranslation;
+        mMiwokTranslation = miwokTranslation;
+    }
+
+    public Word(String defaultTranslation, String miwokTranslation, int imageId) {
+        mDefaultTranslation = defaultTranslation;
+        mMiwokTranslation = miwokTranslation;
+        mImageId = imageId;
+    }
+
+    public int getmImageId() {
+        return mImageId;
     }
 
     public String getDefaultTranslation() {
