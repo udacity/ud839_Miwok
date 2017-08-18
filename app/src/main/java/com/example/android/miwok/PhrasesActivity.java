dd22af0f9a4a0554c4ad6dcc68b2ae483d9562ef
Guilherme Fainer Dalben <guilherme.fainer@gmail.com>
2017-08-17 21:06:31 -0300

    Visual improvements

--- app/src/main/java/com/example/android/miwok/PhrasesActivity.java
+++ app/src/main/java/com/example/android/miwok/PhrasesActivity.java
@@ -25,7 +25,7 @@ public class PhrasesActivity extends AppCompatActivity {
         words.add(new Word("Let’s go." , "yoowutis"));
         words.add(new Word("Come here." , "әnni'nem"));
 
-        WordAdapter wordAdapter = new WordAdapter(this, words);
+        WordAdapter wordAdapter = new WordAdapter(this, words, R.color.category_phrases);
 
         ListView listView = (ListView) findViewById(R.id.list);
 
--- app/src/main/java/com/example/android/miwok/WordAdapter.java
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -3,11 +3,13 @@ package com.example.android.miwok;
 import android.app.Activity;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -18,8 +20,11 @@ import java.util.ArrayList;
 
 public class WordAdapter extends ArrayAdapter<Word> {
 
-    public WordAdapter(Activity context, ArrayList<Word> words) {
+    private int mColor;
+
+    public WordAdapter(Activity context, ArrayList<Word> words, int color) {
         super(context, 0, words);
+        mColor=color;
     }
 
     @Override
@@ -44,6 +49,9 @@ public class WordAdapter extends ArrayAdapter<Word> {
         } else {
             iconView.setVisibility(View.GONE);
         }
+
+        int color = ContextCompat.getColor(getContext(), mColor);
+        listItemView.findViewById(R.id.word_layout).setBackgroundColor(color);
 
         return listItemView;
     }
