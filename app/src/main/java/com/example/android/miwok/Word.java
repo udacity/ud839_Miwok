2ec15c4460a49b7835f6ef0552e0b918adb7ac6d
Victor Durojaiye <durojaiyevic@gmail.com>
2017-12-21 16:10:13 +0100

    Added custom ArrayAdapter for NumbersActivity.java

---
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -0,0 +1,22 @@
+package com.example.android.miwok;
+
+/**
+ * Created by D A Victor on 21-Dec-17.
+ */
+public class Word {
+    private String mDefaultTranslation;
+    private String mMiwokTranslation;
+
+    public Word(String defaultTranslation, String miwokTranslation) {
+        mDefaultTranslation = defaultTranslation;
+        mMiwokTranslation = miwokTranslation;
+    }
+
+    public String getDefaultTranslation() {
+        return mDefaultTranslation;
+    }
+
+    public String getMiwokTranslation() {
+        return mMiwokTranslation;
+    }
+}
