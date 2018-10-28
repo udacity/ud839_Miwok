1ecf540fb0f996840d97453205482d3b9fd39987
MayankSingh1993 <chaudharysinghmayank00@gmail.com>
2018-10-29 04:27:50 +0530

    ViewPager is implemented with fragments.

---
+++ app/src/main/java/com/example/android/miwok/SimpleFragmentPagerAdapter.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.android.miwok;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+ * Provides the appropriate {@link Fragment} for a view pager.
+ */
+public class SimpleFragmentPagerAdapter extends FragmentPagerAdapter {
+
+    public SimpleFragmentPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        if (position == 0) {
+            return new NumbersFragment();
+        } else if (position == 1){
+            return new FamilyFragment();
+        } else if (position == 2) {
+            return new ColorsFragment();
+        }else
+        {
+            return new PhrasesFragment();
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return 4;
+    }
+}
