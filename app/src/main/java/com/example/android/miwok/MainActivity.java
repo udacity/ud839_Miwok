e96a6745b5089cf600b0fd576ba1fbd0a011f648
udalearn <education@jrwallaceco.com>
2018-11-02 15:44:52 -0400

    For and while loop added to NumbersActivity to populate TextViews.

--- app/src/main/java/com/example/android/miwok/MainActivity.java
+++ app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -28,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+
         // Set the content of the activity to use the activity_main.xml layout file
         setContentView(R.layout.activity_main);
 
--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -17,6 +17,10 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -24,5 +28,40 @@ public class NumbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+
+        //Create an array list of words for the Numbers activity
+        ArrayList<String> wordNumbers = new ArrayList<String>();
+        wordNumbers.add("one");
+        wordNumbers.add("two");
+        wordNumbers.add("three");
+        wordNumbers.add("four");
+        wordNumbers.add("five");
+        wordNumbers.add("six");
+        wordNumbers.add("seven");
+        wordNumbers.add("eight");
+        wordNumbers.add("nine");
+        wordNumbers.add("ten");
+
+        //Find the root view of the whole layout
+        LinearLayout rootView = (LinearLayout) findViewById(R.id.root_view);
+
+        int i = 0;
+        while(i < wordNumbers.size()){
+            TextView wordView = new TextView(this);
+            wordView.setText(wordNumbers.get(i));
+            rootView.addView(wordView);
+            i++;
+        }
+
+        for(int j = 0; j < wordNumbers.size(); j++){
+            TextView wordView = new TextView(this);
+            wordView.setText(wordNumbers.get(j));
+            rootView.addView(wordView);
+        }
+
+        //LinearLayout rootView = (LinearLayout) findViewById(R.id.root_view);
+        //TextView wordView = new TextView(this);
+        //wordView.setText(wordNumbers.get(0));
+        //rootView.addView(wordView);
     }
 }
