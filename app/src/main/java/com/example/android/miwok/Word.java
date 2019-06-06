027f4d32e8bbf7a53e7dcf0edca6d76878506d5d
usamaeltmsah <usamaeltmsah@yahoo.com>
2019-06-07 00:31:12 +0200

    ListView and ArrayAdapter

---
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -0,0 +1,11 @@
+package com.example.android.miwok;
+
+public class Word {
+    private String mDefaultTranslation;
+    private String mMiwokTranslation;
+
+    public Word(String mDefaultTranslation, String mMiwokTranslation) {
+        this.mDefaultTranslation = mDefaultTranslation;
+        this.mMiwokTranslation = mMiwokTranslation;
+    }
+}
