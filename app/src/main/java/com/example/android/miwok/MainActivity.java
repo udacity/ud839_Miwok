5460081615328d8d77a9d5ce12261496d4ecab84
usamaeltmsah <usamaeltmsah@yahoo.com>
2019-06-06 16:38:31 +0200

    Some Updates

--- app/src/main/java/com/example/android/miwok/MainActivity.java
+++ app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -32,7 +32,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // Find the View that shows the numbers category
-        TextView numbers = (TextView) findViewById(R.id.numbers);
+        TextView numbers = findViewById(R.id.numbers);
 
         // Set a click listener on that View
         numbers.setOnClickListener(new OnClickListener() {
@@ -48,7 +48,7 @@ public class MainActivity extends AppCompatActivity {
         });
 
         // Find the View that shows the family category
-        TextView family = (TextView) findViewById(R.id.family);
+        TextView family = findViewById(R.id.family);
 
         // Set a click listener on that View
         family.setOnClickListener(new OnClickListener() {
@@ -64,7 +64,7 @@ public class MainActivity extends AppCompatActivity {
         });
 
         // Find the View that shows the colors category
-        TextView colors = (TextView) findViewById(R.id.colors);
+        TextView colors = findViewById(R.id.colors);
 
         // Set a click listener on that View
         colors.setOnClickListener(new OnClickListener() {
@@ -80,7 +80,7 @@ public class MainActivity extends AppCompatActivity {
         });
 
         // Find the View that shows the phrases category
-        TextView phrases = (TextView) findViewById(R.id.phrases);
+        TextView phrases = findViewById(R.id.phrases);
 
         // Set a click listener on that View
         phrases.setOnClickListener(new OnClickListener() {
