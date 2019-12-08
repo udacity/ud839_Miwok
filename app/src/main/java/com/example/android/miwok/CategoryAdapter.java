8aba186215986435cf38fdedb440411937b9d58e
Amirhossein-Ab <amirhosseinarbab@gmail.com>
2019-12-09 01:32:28 +0330

    Update CategoryAdapter.java
    importing required androidx classes

--- app/src/main/java/com/example/android/miwok/CategoryAdapter.java
+++ app/src/main/java/com/example/android/miwok/CategoryAdapter.java
@@ -15,35 +15,35 @@
  */
 package com.example.android.miwok;
 
-import android.content.Context;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
 
-/**
- * {@link CategoryAdapter} is a {@link FragmentPagerAdapter} that can provide the layout for
- * each list item based on a data source which is a list of {@link Word} objects.
- */
+import android.content.Context;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
 public class CategoryAdapter extends FragmentPagerAdapter {
 
-    /** Context of the app */
+    /**
+     * Context of the app
+     */
     private Context mContext;
 
     /**
      * Create a new {@link CategoryAdapter} object.
      *
      * @param context is the context of the app
-     * @param fm is the fragment manager that will keep each fragment's state in the adapter
-     *           across swipes.
+     * @param fm      is the fragment manager that will keep each fragment's state in the adapter
+     *                across swipes.
      */
+
+
     public CategoryAdapter(Context context, FragmentManager fm) {
         super(fm);
         mContext = context;
     }
 
-    /**
-     * Return the {@link Fragment} that should be displayed for the given page number.
-     */
+
     @Override
     public Fragment getItem(int position) {
         if (position == 0) {
@@ -64,6 +64,7 @@ public class CategoryAdapter extends FragmentPagerAdapter {
     public int getCount() {
         return 4;
     }
+
 
     @Override
     public CharSequence getPageTitle(int position) {
