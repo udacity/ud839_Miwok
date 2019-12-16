9bef5f92776a19f18ec34bc063f16da37c28d324
Utsav kumar <utsavkc24@gmail.com>
2019-12-16 23:16:43 +0530

    Just normal (NO changes)

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -17,6 +17,11 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -24,5 +29,20 @@ public class NumbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+        ArrayList<String> word=new ArrayList<String>();
+        word.add(0,"One");
+        word.add(1,"Two");
+        word.add(2,"Three");
+        word.add(3,"Four");
+        word.add(4,"Five");
+        word.add(5,"Six");
+        word.add(6,"Seven");
+        word.add(7,"Eight");
+        word.add(8,"Nine");
+        word.add(9,"Ten");
+        LinearLayout rootView=(LinearLayout)findViewById(R.id.rootView);
+        TextView wordView=new TextView(this);
+        wordView.setText(word.get(0));
+        rootView.addView(wordView);
     }
 }
