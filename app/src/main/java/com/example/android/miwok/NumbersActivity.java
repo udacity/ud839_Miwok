9e677cc405dc61f9b0735fc3b36cb018fab441bf
Suraj sahani <surajkumarsahani1997@gmail.com>
2020-07-11 15:36:27 +0530

    ADding loop

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -17,6 +17,12 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -24,5 +30,38 @@ public class NumbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+
+        //Create an array of words
+        ArrayList<String> words = new ArrayList<String>();
+
+        words.add("one");
+        words.add("two");
+        words.add("three");
+        words.add("four");
+        words.add("five");
+        words.add("six");
+        words.add("seven");
+        words.add("eight");
+        words.add("nine");
+        words.add("ten");
+
+        //Find the root view of the whole layout
+        LinearLayout rootView = (LinearLayout)findViewById(R.id.rootView);
+
+        for(int index = 0; index < words.size(); index++);
+        {
+            //Create a new TextView
+            TextView wordView = new TextView(this);
+
+            //Set the text to be word at the current index
+            wordView.setText(words.get(index));
+
+            //Add this TextView as another child to the root view of this layout
+            rootView.addView(wordView);
+
+
+        }
     }
 }
+
+
