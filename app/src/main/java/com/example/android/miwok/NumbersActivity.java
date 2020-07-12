3aa9528086d32400dd7c3a12ed0fd7337b161f6e
Suraj sahani <surajkumarsahani1997@gmail.com>
2020-07-12 11:17:49 +0530

    adding logs

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -42,7 +42,7 @@ public class NumbersActivity extends AppCompatActivity {
         words.add("nine");
         words.add("ten");
 
-        //Create an array of words
+        //Verify the content of ArrayList by printing out each Array
         Log.v("MainActivity","Word at index 0: " + words.get(0));
         Log.v("MainActivity","Word at index 0: " + words.get(1));
         Log.v("MainActivity","Word at index 0: " + words.get(2));
