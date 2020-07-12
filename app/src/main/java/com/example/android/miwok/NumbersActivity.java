35f50888526337cc2c6afa4ab130d6474fdd96bf
Suraj sahani <surajkumarsahani1997@gmail.com>
2020-07-12 11:10:27 +0530

    Implementing ArrayList

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -15,15 +15,10 @@
  */
 package com.example.android.miwok;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.GridView;
-import android.widget.LinearLayout;
 import android.widget.ListView;
-import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -47,25 +42,16 @@ public class NumbersActivity extends AppCompatActivity {
         words.add("nine");
         words.add("ten");
 
-        // Create an {@link ArrayAdapter}, whose data source is a list of Strings. The
-        // adapter knows how to create layouts for each item in the list, using the
-        // simple_list_item_1.xml layout resource defined in the Android framework.
-        // This list item layout contains a single {@link TextView}, which the adapter will set to
-        // display a single word.
-        ArrayAdapter<String> itemsAdapter =
-                new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, words);
-
-        // Find the {@link ListView} object in the view hierarchy of the {@link Activity}.
-        // There should be a {@link ListView} with the view ID called list, which is declared in the
-        // activity_numbers.xml layout file.
-        GridView listView = (GridView) findViewById(R.id.list);
-
-        // Make the {@link ListView} use the {@link ArrayAdapter} we created above, so that the
-        // {@link ListView} will display list items for each word in the list of words.
-        // Do this by calling the setAdapter method on the {@link ListView} object and pass in
-        // 1 argument, which is the {@link ArrayAdapter} with the variable name itemsAdapter.
-        listView.setAdapter(itemsAdapter);
+        //Verify  the contents of the ArrayList by printing out each ArrayList element to the logs
+        Log.v("MainActivity","Word at index 0: " + words.get(0));
+        Log.v("MainActivity","Word at index 0: " + words.get(1));
+        Log.v("MainActivity","Word at index 0: " + words.get(2));
+        Log.v("MainActivity","Word at index 0: " + words.get(3));
+        Log.v("MainActivity","Word at index 0: " + words.get(4));
+        Log.v("MainActivity","Word at index 0: " + words.get(5));
+        Log.v("MainActivity","Word at index 0: " + words.get(6));
+        Log.v("MainActivity","Word at index 0: " + words.get(7));
+        Log.v("MainActivity","Word at index 0: " + words.get(8));
+        Log.v("MainActivity","Word at index 0: " + words.get(9));
     }
 }
-
-
