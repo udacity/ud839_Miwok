4ce00ff1c10bb658d693b8b11a8858e3470305d4
sudkul <sudhanshu.kulshrestha@gmail.com>
2022-02-03 13:09:45 +0530

    Upgrade Gradle and dependencies to AndroidX

--- app/src/main/java/com/example/android/miwok/FamilyActivity.java
+++ app/src/main/java/com/example/android/miwok/FamilyActivity.java
@@ -18,13 +18,16 @@ package com.example.android.miwok;
 import android.content.Context;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import java.util.ArrayList;
+
+// import android.support.v7.app.AppCompatActivity;
 
 public class FamilyActivity extends AppCompatActivity {
 
